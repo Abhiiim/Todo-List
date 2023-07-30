eafdd6697023fd0011e472bd4e30aaf3868c15f5
Abhishek Kumar <abhishekkumar500a@gmail.com>
2023-07-30 12:21:50 +0530

    Added Drag and Drop functions in localstorage after this

--- drag-drop.js
+++ drag-drop.js
@@ -1,10 +1,7 @@
 let items = document.querySelectorAll('.item');
-// let listItems = document.querySelector('list');
-
-console.log((list));
 
 items.forEach(item => {
-    item.draggable = true;
+  item.draggable = true;
 });
 
 list.addEventListener('dragstart', dragStart);
@@ -16,32 +13,51 @@ list.addEventListener('drop', dragDrop);
 let dragItem = null;
 
 function dragStart(event) {
-    console.log('drag started');
-    dragItem = event.target;
+  console.log('drag started');
+  dragItem = event.target;
 }
 
 function dragOver(event) {
-    event.preventDefault()
-    console.log('drag over');
+  event.preventDefault()
+  console.log('drag over');
 }
 
 function dragDrop(event) {
-    console.log('drag dropped');
-    event.preventDefault();
-    console.log(dragItem);
-    if (dragItem) {
-        const targetItem = event.target.closest(".item");
-        console.log(targetItem);
-        if (targetItem !== dragItem) {
-            const targetIndex = Array.from(list.children).indexOf(targetItem);
-            const draggedIndex = Array.from(list.children).indexOf(dragItem);
+  console.log('drag dropped');
+  event.preventDefault();
+  console.log(dragItem);
+  if (dragItem) {
+    const targetItem = event.target.closest(".item");
+    console.log(targetItem);
+    if (targetItem !== dragItem) {
+      const targetIndex = Array.from(list.children).indexOf(targetItem);
+      const draggedIndex = Array.from(list.children).indexOf(dragItem);
 
-            if (targetIndex > draggedIndex) {
-                list.insertBefore(dragItem, targetItem.nextSibling);
-            } else {
-                list.insertBefore(dragItem, targetItem);
-            }
+      // console.log(targetIndex);
+      // console.log(draggedIndex);
+
+      let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+      let draggedItem = tasks[draggedIndex];
+
+      console.log(tasks);
+
+      if (targetIndex > draggedIndex) {
+        list.insertBefore(dragItem, targetItem.nextSibling);
+        for (let i = targetIndex; i >= draggedIndex; i--) {
+          let swap = tasks[i];
+          tasks[i] = draggedItem;
+          draggedItem = swap;
         }
-        dragItem = null;
+      } else {
+        list.insertBefore(dragItem, targetItem);
+        for (let i = targetIndex; i <= draggedIndex; i++) {
+          let swap = tasks[i];
+          tasks[i] = draggedItem;
+          draggedItem = swap;
+        }
+      }
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
+    dragItem = null;
+  }
 }
